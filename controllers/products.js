fd66f336ed9196626d2135f011a8578d86c8c166
Duc Hong <duc.hong@tuni.fi>
2020-11-09 21:45:48 +0200

    fix previous commit

--- controllers/products.js
+++ controllers/products.js
@@ -4,11 +4,10 @@
  * @param {http.ServerResponse} response
  */
 const { sendJson } = require('../utils/responseUtils');
-const responseUtils = require('../utils/responseUtils');
 
 const getAllProducts = async response => {
   const productData = await require('../products.json').map(product => ({ ...product }));
-  responseUtils.sendJson(response, productData);
+  sendJson(response, productData);
 };
 
 module.exports = { getAllProducts };
