94132600d63e72c2f563525680fc6c5275930c0d
Duc Hong <duc.hong@tuni.fi>
2020-11-12 02:58:15 +0200

    add test 10.3

---
+++ test/own/own.js
@@ -0,0 +1,77 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Test numerical operations', () =>{
+    describe('examine 1+1 = 3', ()=>
+    {
+        it('1+1 should be 3', ()=>{
+            expect(1+1).to.equal(3);
+        });
+    });
+    describe('examine 1*1 = 3', ()=>
+    {
+        it('1*1 should be 3', ()=>{
+            expect(1*1).to.equal(3);
+        });
+    });
+    describe('examine undefined === true', ()=>
+    {
+        it('undefined should be true', ()=>{
+            expect(undefined).to.equal(true);
+        });
+    });
+    describe('examine 3/2 = 1', ()=>
+    {
+        it('expect 3/2 = 1', ()=>{
+            expect(3/2).to.equal(1);
+        });
+    });
+    describe('examine 3/2 = 1', ()=>
+    {
+        it('expect 3/2 = 1', ()=>{
+            expect(3/2).to.equal(1);
+        });
+    });
+    describe('examine 3/2 = 1', ()=>
+    {
+        it('expect 3/2 = 1', ()=>{
+            expect(3/2).to.equal(1);
+        });
+    });
+    describe('examine 3/2 = 1', ()=>
+    {
+        it('expect 3/2 = 1', ()=>{
+            expect(3/2).to.equal(1);
+        });
+    });
+    describe('examine 3/2 = 1', ()=>
+    {
+        it('expect 3/2 = 1', ()=>{
+            expect(3/2).to.equal(1);
+        });
+    });
+    describe('examine 3/2 = 1', ()=>
+    {
+        it('expect 3/2 = 1', ()=>{
+            expect(3/2).to.equal(1);
+        });
+    });
+    describe('examine 3/2 = 1', ()=>
+    {
+        it('expect 3/2 = 1', ()=>{
+            expect(3/2).to.equal(1);
+        });
+    });
+    describe('examine 3/2 = 1', ()=>
+    {
+        it('expect 3/2 = 1', ()=>{
+            expect(3/2).to.equal(1);
+        });
+    });
+    describe('examine 3/2 = 1', ()=>
+    {
+        it('expect 3/2 = 1', ()=>{
+            expect(3/2).to.equal(1);
+        });
+    });
+})
